b7b8585693cec0ccc8c008235b33873b12ce8be6
nayon117 <hasibul.nayon1@gmail.com>
2024-03-26 05:57:57 +0600

    add form from shadcn

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
 
 const AddTransformationTypePage = ({params:{type}}:SearchParamProps) => {
 
     const tranformation = transformationTypes[type]
     return(
-        <div>
+        <>
              <Header title={tranformation.title} subtitle={tranformation.subTitle} />
-        </div>
+             
+             {/* Transformation form */}
+             <TransformationForm/>
+        </>
     )
 }
 export default AddTransformationTypePage;
