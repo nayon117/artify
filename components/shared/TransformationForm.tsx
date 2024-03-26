745f95c714c664d5434fbcf89e045b90573b1390
nayon117 <hasibul.nayon1@gmail.com>
2024-03-26 09:58:20 +0600

    create select field

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -12,26 +12,49 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Input } from "@/components/ui/input";
-import { defaultValues } from "@/constants";
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants";
+import { CustomField } from "./CustomField";
+import { useState } from "react";
+import { AspectRatioKey } from "@/lib/utils";
 
-const formSchema = z.object({
-   title:z.string(),
-   aspectRatio:z.string().optional(),
-   color:z.string().optional(),
-   prompt:z.string().optional(),
-   publicId:z.string()
-  });
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
 
-const TransformationForm = ({action,data=null}:TransformationFormProps) => {
+const TransformationForm = ({
+  action,
+  data = null,
+  userId,
+  type,
+  creditBalance,
+}: TransformationFormProps) => {
 
-  const intialValues = data && action==="Update" ? {
-    title:data.title,
-    aspectRatio:data.aspectRatio,
-    color:data.color,
-    prompt:data.prompt,
-    publicId:data.publicId
-  } : defaultValues
+  const transformationType = transformationTypes[type]
+  const [image,setImage] = useState(data)
+  const [newTransformation,setNewTransformation] = useState<Transformations | null>(null)
+  const intialValues =
+    data && action === "Update"
+      ? {
+          title: data.title,
+          aspectRatio: data.aspectRatio,
+          color: data.color,
+          prompt: data.prompt,
+          publicId: data.publicId,
+        }
+      : defaultValues;
 
   // 1. Define form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -43,10 +66,55 @@ const TransformationForm = ({action,data=null}:TransformationFormProps) => {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
   }
+
+
+  const onSelectFieldHandler = (value:string, onChangeField:(value:string)=>void) =>{
+
+  }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        
+        <CustomField
+          control={form.control}
+          name="title"
+          formLabel="Image Title"
+          render={({ field }) => (
+            <Input
+              {...field}
+              placeholder="Image Title"
+              className="input-field"
+            />
+          )}
+        />
+
+        {type === "fill" && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select 
+              onValueChange={(value) => onSelectFieldHandler(value,field.onChange)}
+              >
+                <SelectTrigger className="select-field">
+                  <SelectValue placeholder="select size" />
+                </SelectTrigger>
+                <SelectContent>
+                  {
+                    Object.keys(aspectRatioOptions).map((key)=>(
+                      <SelectItem key={key} value={key} className="select-item">
+                        {aspectRatioOptions[key  as AspectRatioKey].label}
+                      </SelectItem>
+                    
+                    ))
+                  }
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
       </form>
     </Form>
   );
